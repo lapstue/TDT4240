220e1ecbba503de71aa796db39ba85426700bb3a
Kåre Birger Lapstuen <lapstue@gmail.com>
2015-01-25 23:26:55 +0100

    Make TDT4240 load Exercise 1 instead of MainMenu
    for debugging.

--- core/src/no/ntnu/kaarebl/TDT4240Game.java
+++ core/src/no/ntnu/kaarebl/TDT4240Game.java
@@ -6,17 +6,21 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class TDT4240Game extends Game {
 
     public SpriteBatch batch;
     public BitmapFont font;
+    public Stage stage;
 
     public void create() {
+        Constants.screenWidt = Gdx.graphics.getWidth();
+        Constants.screenHeight = Gdx.graphics.getHeight();
+        stage = new Stage();
         batch = new SpriteBatch();
-        //Use LibGDX's default Arial font.
         font = new BitmapFont();
-        this.setScreen(new MainMenuScreen(this));
+        this.setScreen(new Exercise1(this));
     }
 
     public void render() {
