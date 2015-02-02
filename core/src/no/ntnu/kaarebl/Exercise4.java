82b2c8837047a1372c2b3e6c9171f62af064f0bc
Kåre Birger Lapstuen <lapstue@gmail.com>
2015-02-02 14:20:07 +0100

    Comment and clean up the code

--- core/src/no/ntnu/kaarebl/Exercise4.java
+++ core/src/no/ntnu/kaarebl/Exercise4.java
@@ -1,14 +1,10 @@
 package no.ntnu.kaarebl;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.viewport.ScalingViewport;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 /**
@@ -22,7 +18,6 @@ public class Exercise4 implements Screen, InputProcessor {
     private int player2Score;
     private Paddle player1Paddle;
     private Paddle player2Paddle;
-    private SpriteBatch batch;
     private float scaling;
 
     public Exercise4(){
@@ -31,19 +26,23 @@ public class Exercise4 implements Screen, InputProcessor {
 
     public void create() {
         stage = new Stage(new StretchViewport(480,800));
+        //Scaling factor used to translate screen coordinates to stage coordinates
         scaling = 480/Constants.screenWidt;
-        batch = new SpriteBatch();
         ball = new Ball();
         player1Score = 0;
         player2Score = 0;
+        //Set player1Paddle to be touch controlled and set start postion
         player1Paddle = new Paddle(false,ball,240,75);
+        //player2Paddle is computer controlled
         player2Paddle = new Paddle(true,ball,240,725);
         stage.addActor(ball);
         stage.addActor(player1Paddle);
         stage.addActor(player2Paddle);
+        //Makes sure the Exercise4 class handles touch inputs
         Gdx.input.setInputProcessor(this);
     }
 
+    //Checks whether player 1 or player 2 paddle overlaps the ball. In that case trigger bounce
     public void checkCollision(){
         if(player1Paddle.bounds.overlaps(ball.bounds)){
             ball.collision();
@@ -53,12 +52,15 @@ public class Exercise4 implements Screen, InputProcessor {
         }
     }
 
+    //Checks if the ball has left either screen in y direction, in that case increment score
     public void checkIfScore(){
+        //Player 2 has scored a point. Award point and reset the ball
         if(ball.getY()<0){
             player2Score = player2Score + 1;
             System.out.println(player2Score);
             ball.setPosition(240,400);
         }
+        //Player1 has scored a point. Award point and reset the ball
         if(ball.getY()>800){
             player1Score = player1Score + 1;
             System.out.println(player1Score);
@@ -74,9 +76,13 @@ public class Exercise4 implements Screen, InputProcessor {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        //First check if a collision is to be triggered
         checkCollision();
+        //Check that the ball has not left the screen, if it has score and reset
         checkIfScore();
+        //Move the on screen components according to the deltaT elapsed since last frame
         stage.act(delta);
+        //Call the draw function of all the actors added to the scene.
         stage.draw();
     }
 
@@ -132,7 +138,9 @@ public class Exercise4 implements Screen, InputProcessor {
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
+        //Tell the player1Paddle to move to the coordinate of touch.
         player1Paddle.moveToTouch(screenX*scaling);
+        //Remember to return true to indicate that the input has been processed.
         return true;
     }
 
--- core/src/no/ntnu/kaarebl/Paddle.java
+++ core/src/no/ntnu/kaarebl/Paddle.java
@@ -25,24 +25,33 @@ public class Paddle extends Actor {
         setWidth(texture.getWidth());
         setX(xPos);
         setY(yPos);
+        //Rotates the paddle by 270 degrees. 90 degrees makes the collision rectangle and the sprite
+        //not match.
         rotateBy(270);
         bounds=new Rectangle((int)getX(), (int)getY(), (int)getHeight(), (int)getWidth());
+        //Not strictly necessary, but let's do it anyway.
         setBounds();
     }
+    //Set collision bounds. Must be called each frame, in case the paddle has moved since last time.
     public void setBounds(){
         bounds.set(getX(),getY(),getHeight(),getWidth());
     }
 
+    //Reads the xPos of the ball and sets the paddle to the same position.
     public void moveToBall(Ball ball){
+        //Correct for the fact that the sprite's (0,0) is the bottom left of the sprite
+        //remember that since the sprite has been rotated by 270 degrees, the height is not width
         if(ball.getX()+getHeight()>480){
             setX(480-getHeight());
         }
+        //No need to correct for (0,0) of the sprite.
         else{
             setX(ball.getX());
         }
         setBounds();
     }
 
+    //Responds to the xPos of a touchDragged event and moves the paddle accordingly.
     public void moveToTouch(float x){
         if(x+getHeight()>480){
             x = 480-getHeight();
@@ -56,6 +65,7 @@ public class Paddle extends Actor {
 
     @Override
     public void act(float delta) {
+        //If the paddle is controlled by the computer, let the paddle move to the balls xPos
         if(isComputer){
             moveToBall(ball);
         }
@@ -64,6 +74,8 @@ public class Paddle extends Actor {
 
     @Override
     public void draw(Batch batch, float parentAlpha){
+        //Draw the sprite at the given coordinates. Remember to include getRotation to make sure the
+        //rotation is set correctly.
         batch.draw(sprite,getX(),getY(),getOriginX(),getOriginY(),getWidth(),getHeight(),1,1,getRotation());
     }
 }
