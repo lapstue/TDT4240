a2adc14bcb5334d2edf4c4a26813a8468bbf60fb
Kåre Birger Lapstuen <lapstue@gmail.com>
2015-02-02 14:21:42 +0100

    Remove imports we don't need

--- core/src/no/ntnu/kaarebl/Exercise3.java
+++ core/src/no/ntnu/kaarebl/Exercise3.java
@@ -1,14 +1,12 @@
 package no.ntnu.kaarebl;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class Exercise3 implements Screen{
--- core/src/no/ntnu/kaarebl/HelicopterAnimated.java
+++ core/src/no/ntnu/kaarebl/HelicopterAnimated.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
