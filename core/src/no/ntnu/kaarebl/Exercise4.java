5ae3920aeabfc47a17be3f03692cfea83a3c2721
Kåre Birger Lapstuen <lapstue@gmail.com>
2015-02-02 12:51:20 +0100

    Implement Input for paddles

--- core/src/no/ntnu/kaarebl/Exercise4.java
+++ core/src/no/ntnu/kaarebl/Exercise4.java
@@ -1,6 +1,8 @@
 package no.ntnu.kaarebl;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -12,7 +14,7 @@ import com.badlogic.gdx.utils.viewport.StretchViewport;
 /**
  * Created by kbl on 2/1/15.
  */
-public class Exercise4 implements Screen {
+public class Exercise4 implements Screen, InputProcessor {
 
     private Stage stage;
     private Ball ball;
@@ -21,6 +23,7 @@ public class Exercise4 implements Screen {
     private Paddle player1Paddle;
     private Paddle player2Paddle;
     private SpriteBatch batch;
+    private float scaling;
 
     public Exercise4(){
         create();
@@ -28,15 +31,17 @@ public class Exercise4 implements Screen {
 
     public void create() {
         stage = new Stage(new StretchViewport(480,800));
+        scaling = 480/Constants.screenWidt;
         batch = new SpriteBatch();
         ball = new Ball();
         player1Score = 0;
         player2Score = 0;
-        player1Paddle = new Paddle(false,ball,150,50);
-        player2Paddle = new Paddle(true,ball,200,300);
+        player1Paddle = new Paddle(false,ball,240,75);
+        player2Paddle = new Paddle(true,ball,240,725);
         stage.addActor(ball);
         stage.addActor(player1Paddle);
         stage.addActor(player2Paddle);
+        Gdx.input.setInputProcessor(this);
     }
 
     public void checkCollision(){
@@ -45,6 +50,19 @@ public class Exercise4 implements Screen {
         }
         if(player2Paddle.bounds.overlaps(ball.bounds)){
             ball.collision();
+        }
+    }
+
+    public void checkIfScore(){
+        if(ball.getY()<0){
+            player2Score = player2Score + 1;
+            System.out.println(player2Score);
+            ball.setPosition(240,400);
+        }
+        if(ball.getY()>800){
+            player1Score = player1Score + 1;
+            System.out.println(player1Score);
+            ball.setPosition(240,400);
         }
     }
     @Override
@@ -57,6 +75,7 @@ public class Exercise4 implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         checkCollision();
+        checkIfScore();
         stage.act(delta);
         stage.draw();
     }
@@ -84,5 +103,46 @@ public class Exercise4 implements Screen {
     @Override
     public void dispose() {
 
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        player1Paddle.moveToTouch(screenX*scaling);
+        return true;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
     }
 }
