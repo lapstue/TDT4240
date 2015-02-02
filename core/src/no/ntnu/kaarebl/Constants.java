b62def5ede2e1a32467556715b5b79249adffeb1
Kåre Birger Lapstuen <lapstue@gmail.com>
2015-02-02 18:56:38 +0100

    Fix stupid and old typo

--- core/src/no/ntnu/kaarebl/Constants.java
+++ core/src/no/ntnu/kaarebl/Constants.java
@@ -1,9 +1,6 @@
 package no.ntnu.kaarebl;
 
-/**
- * Created by kbl on 1/24/15.
- */
 public class Constants {
     public static float screenHeight;
-    public static float screenWidt;
+    public static float screenWidth;
 }
